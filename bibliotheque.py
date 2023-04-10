f197c78873784c8d501425aa2e0e1434d48e12b8
Ghack81 <herve-mention@hotmail.com>
2023-04-10 21:44:45 +0200

    Add for proc server

--- bibliotheque.py
+++ bibliotheque.py
@@ -52,9 +52,10 @@ def add_book():
 
     return render_template('add_book.html')
 
-"""if __name__ == '__main__':
-    app.run(debug=True)"""
-    
+if __name__ == '__main__':
+    app.run(debug=True)
+"""   
 if __name__ == "__main__":
     from waitress import serve
     serve(app, host="0.0.0.0", port=8080)
+"""
